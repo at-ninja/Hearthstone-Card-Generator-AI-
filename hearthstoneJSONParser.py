b529774010a5d286597bc7f43b9b9d1b4360df76
Andrew Thomas <andrew@cjthomas.org>
2017-04-02 13:58:12 -0500

    Filtered hearthstone Parser to only show scored cards. Also added races to logged data

--- hearthstoneJSONParser.py
+++ hearthstoneJSONParser.py
@@ -3,27 +3,35 @@
 
 import json
 
-def main(filename : str) -> None:
+def main(filename : str, ratingsFilename: str) -> None:
 
     with open(filename) as fp:
 
         cards = json.load(fp)
 
-    cards.sort(key=lambda card: card["name"].lower())
+    with open(ratingsFilename) as fp:
+
+        scores = json.load(fp)['Cards']
+    
+    scores = list(map(lambda card: card['Name'].lower(), scores))
+
+    cards.sort(key=lambda card: card['name'].lower())
     
     with open(filename + '_formatted.txt', 'w') as fp:
         for card in cards:
-            attack = card.get('attack', '')
-            cardClass = card.get('cardClass', '')
-            cost = card.get('cost', '')
-            health = card.get('health', '')
-            text = card.get('text', '').replace('\n', ' ')
-            cardType = card.get('type', '')
-            name = card.get('name', '')
-            rarity = card.get('rarity', '')
+            if card['name'].lower() in scores:
+                name = card.get('name', '')
+                attack = card.get('attack', '')
+                cardClass = card.get('cardClass', '')
+                cost = card.get('cost', '')
+                health = card.get('health', '')
+                text = card.get('text', '').replace('\n', ' ').replace('<b>', '').replace('</b>', '').replace('[x]', '')
+                cardType = card.get('type', '')
+                rarity = card.get('rarity', '')
+                tribe = card.get('race', '')
 
-            fp.write('|0{0}|1{1}|2{2}|3{3}|4{4}|5{5}|6{6}|7{7}\n'.format(attack, cardClass, cost, health, text, cardType, name, rarity))
+                fp.write('|0{0}|1{1}|2{2}|3{3}|4{4}|5{5}|6{6}|7{7}|8{8}\n'.format(attack, cardClass, cost, health, text, cardType, name, rarity, tribe))
 
 if __name__=='__main__':
     import sys
-    sys.exit(main(sys.argv[1]))
+    sys.exit(main(sys.argv[1], sys.argv[2]))

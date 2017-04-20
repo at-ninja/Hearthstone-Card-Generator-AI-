8120e9578e6941df9f957c807483a23420b152b7
Andrew Thomas <andrew@cjthomas.org>
2017-04-20 14:57:41 -0500

    Added validation split

--- RNN_Card_Generator.py
+++ RNN_Card_Generator.py
@@ -77,7 +77,7 @@ def create_rnn(X, y, model):
     checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
     callbacks_list = [checkpoint]
 
-    model.fit(X, y, epochs=50, batch_size=50, callbacks=callbacks_list)
+    model.fit(X, y, validation_split=0.1, epochs=50, batch_size=50, callbacks=callbacks_list)
 
 
 def generate(dataX, int_to_char, n_vocab, model):
